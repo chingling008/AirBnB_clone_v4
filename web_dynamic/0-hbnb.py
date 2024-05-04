ddde5ed6b1349fa739e2927d5a370951b9be71b9
chingling008 <chinglingmadness@gmail.com>
2024-05-04 10:07:49 -0700

    task1fix

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -6,11 +6,12 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
-from flask import Flask, render_template, uuid
-
+from flask import Flask, render_template
+import uuid
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
-# Import uuid module for generating unique identifiers
 
 @app.teardown_appcontext
 def close_db(error):
@@ -18,33 +19,20 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb/', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
-    # Generate a unique cache identifier using uuid.uuid4()
-    cache_id = str(uuid.uuid4())
-
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    places = storage.all('Place')
+    cache_id = uuid.uuid4()
     return render_template('0-hbnb.html',
-                           states=st_ct,
+                           states=states,
                            amenities=amenities,
                            places=places,
-                           cache_id=cache_id)  # Pass cache_id to template
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
-
