5e29e509209b22eb5a39205c7dda2454314a79cb
chingling008 <chinglingmadness@gmail.com>
2024-05-05 13:29:50 +0000

    allMandortorytask

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,13 @@
-const $ = window.$;
+t $ = window.$;
 $(document).ready(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (response) {
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
   const amenitiesId = {};
   $('input[type="checkbox"]').click(function () {
     if ($(this).prop('checked') === true) {
@@ -7,6 +15,6 @@ $(document).ready(function () {
     } else if ($(this).prop('checked') === false) {
       delete amenitiesId[$(this).attr('data-id')];
     }
-    $('div.amenities H4').text(Object.values(amenitiesId).join(', '));
+    $('.amenities H4').text(Object.values(amenitiesId).join(', '));
   });
 });
